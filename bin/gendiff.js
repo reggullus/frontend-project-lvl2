eef69bbd83f2fabc59e3572616e5330ea730132d
reggullus <Reggullus@yandex.ru>
2022-03-08 12:43:45 +0500

    added gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,5 @@ const program = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.0.1', '-V, --version', 'output the version number')
+.option('-f, --format <type>','output format')
 program.parse();
