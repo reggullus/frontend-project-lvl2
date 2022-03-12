fa4234bd3f0704a1becac92a17c64f41d3152a85
reggullus <Reggullus@yandex.ru>
2022-03-12 21:53:24 +0500

    fix bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-const program = new Command();
+import { program } from 'commander';
+import getFileСompare from '../index.js';
 
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.0.1', '-V, --version', 'output the version number')
 .option('-f, --format <type>','output format')
+.arguments('<filepath1> <filepath2>')
+.action((filepath1, filepath2) => {
+    const diff = getFileСompare(filepath1, filepath2);
+    console.log(diff);
+  });
 program.parse();
