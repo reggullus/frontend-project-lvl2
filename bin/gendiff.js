e2c88bfbb94d3dae25468e024c03edfc537f4a78
reggullus <Reggullus@yandex.ru>
2022-03-13 10:41:39 +0500

    fix file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,11 +3,11 @@ import { program } from 'commander';
 import getFileСompare from '../index.js';
 
 program
-.description('Compares two configuration files and shows a difference.')
-.version('0.0.1', '-V, --version', 'output the version number')
-.option('-f, --format <type>','output format')
-.arguments('<filepath1> <filepath2>')
-.action((filepath1, filepath2) => {
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
     const diff = getFileСompare(filepath1, filepath2);
     console.log(diff);
   });
