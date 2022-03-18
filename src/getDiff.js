21121c6a8a7355f54a706759cb101845cfff537b
reggullus <Reggullus@yandex.ru>
2022-03-18 16:34:34 +0500

    add getDiff

---
+++ src/getDiff.js
@@ -0,0 +1,16 @@
+import { getFormat, readFile } from './getPath.js';
+import parsersFilter from './parsers.js';
+import getFileСompare from './getTree.js';
+import formatData from './formater/index.js';
+
+const getDiff = (data1, data2, format = 'stylish') => {
+  const format1 = getFormat(data1);
+  const format2 = getFormat(data2);
+  const getPath1 = readFile(data1);
+  const getPath2 = readFile(data2);
+  const file1 = parsersFilter(getPath1, format1);
+  const file2 = parsersFilter(getPath2, format2);
+  const compareData = getFileСompare(file1, file2);
+  return formatData(compareData, format);
+};
+export default getDiff;
