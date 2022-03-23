c0285c59dbf72f7df34728172e8ff6a9079c46fa
reggullus <Reggullus@yandex.ru>
2022-03-23 17:35:28 +0500

    change test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,39 +2,30 @@ import { test, expect, describe } from '@jest/globals';
 import getDiff from '../index.js';
 import { readFile } from '../src/getPath.js';
 
-const jsonFile1 = 'filepath1.json';
-const jsonFile2 = 'filepath2.json';
-const ymlFile1 = 'filepath1.yml';
-const ymlFile2 = 'filepath2.yml';
-const result = readFile('flat.txt');
-const plain = readFile('plain.txt');
-const json = readFile('json.txt');
-
-describe(('stylishTest'), () => {
-  test('json', () => {
-    expect(getDiff(jsonFile1, jsonFile2)).toEqual(result);
-  });
-  test('yml', () => {
-    expect(getDiff(ymlFile1, ymlFile2)).toEqual(result);
-  });
-});
-
-describe(('plainTest'), () => {
-  test('json', () => {
-    expect(getDiff(jsonFile1, jsonFile2, 'plain')).toEqual(plain);
-  });
-  test('yml', () => {
-    expect(getDiff(ymlFile1, ymlFile2, 'plain')).toEqual(plain);
-  });
-});
-
-describe('jsonTest', () => {
-  test(('json'), () => {
-    expect(getDiff(jsonFile1, jsonFile2, 'json')).toEqual(json);
-  });
-  test('yml', () => {
-    expect(getDiff(ymlFile1, ymlFile2, 'json')).toEqual(json);
+describe.each([
+  {
+    file1: 'filepath1.json', file2: 'filepath2.json', format: 'stylish', result: 'flat.txt',
+  },
+  {
+    file1: 'filepath1.yml', file2: 'filepath2.yml', format: 'stylish', result: 'flat.txt',
+  },
+  {
+    file1: 'filepath1.json', file2: 'filepath2.json', format: 'plain', result: 'plain.txt',
+  },
+  {
+    file1: 'filepath1.yml', file2: 'filepath2.yml', format: 'plain', result: 'plain.txt',
+  },
+  {
+    file1: 'filepath1.json', file2: 'filepath2.json', format: 'json', result: 'json.txt',
+  },
+  {
+    file1: 'filepath1.yml', file2: 'filepath2.yml', format: 'json', result: 'json.txt',
+  },
+])('.add($file1, $file2, $format)', ({
+  file1, file2, format, result,
+}) => {
+  test(`test${format}`, () => {
+    const expected = readFile(result);
+    expect(getDiff(file1, file2, format)).toEqual(expected);
   });
 });
-
-console.log(getDiff(jsonFile1, jsonFile2, 'json'));
