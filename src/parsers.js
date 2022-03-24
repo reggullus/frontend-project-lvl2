08c4fc840793a0cdc3573df693f09e5c7d78758a
reggullus <Reggullus@yandex.ru>
2022-03-24 10:44:43 +0500

    add yaml case

--- src/parsers.js
+++ src/parsers.js
@@ -6,8 +6,10 @@ const parsersFilter = (data, format) => {
       return JSON.parse(data);
     case 'yml':
       return yaml.load(data);
+    case 'yaml':
+      return yaml.load(data);
     default:
-      throw new Error(`Unknown extension:${format}!`);
+      throw new Error(`Unknown format: ${format}!`);
   }
 };
 export default parsersFilter;
